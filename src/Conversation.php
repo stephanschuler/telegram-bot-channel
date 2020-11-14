91899aea40b842e6154664172ced21407fac3464
Stephan Schuler <stephan.schuler@netlogix.de>
2020-11-15 00:13:52 +0100

    Conversations are filter objects

--- src/Conversation.php
+++ src/Conversation.php
@@ -4,35 +4,28 @@ declare(strict_types=1);
 namespace StephanSchuler\TelegramBot\Channel;
 
 use StephanSchuler\Events\Event;
+use StephanSchuler\Events\Events;
+use StephanSchuler\Events\Modification\Filter;
 use StephanSchuler\TelegramBot\Api\Connection;
 use StephanSchuler\TelegramBot\Api\Sendable\SendMessage;
 use StephanSchuler\TelegramBot\Api\Types\Chat;
 use StephanSchuler\TelegramBot\Api\Types\Message;
-use StephanSchuler\Events\Events;
 
-final class Conversation
+final class Conversation implements Filter
 {
     private $channel;
     private $events;
     private $chat;
+    private $user;
 
     private function __construct(Channel $channel, Chat $chat, Chat $user)
     {
-        $filter = static function (Event $event) use ($chat, $user) {
-            if (!$event instanceof Update) {
-                return false;
-            }
-            $data = $event->toArray();
-            $messageChat = Chat::forUser($data['message']['chat']['id'] ?? 0);
-            $messageUser = Chat::forUser($data['message']['from']['id'] ?? 0);
-            return $messageChat->equals($chat)
-                && $messageUser->equals($user);
-        };
         $this->events = $channel
             ->getEvents()
-            ->filter($filter);
+            ->filter($this);
         $this->channel = $channel;
         $this->chat = $chat;
+        $this->user = $user;
     }
 
     public function getEvents(): Events
@@ -58,5 +51,17 @@ final class Conversation
     public static function create(Channel $channel, Chat $chat, Chat $user): self
     {
         return new static($channel, $chat, $user);
+    }
+
+    public function filterEvent(Event $event): bool
+    {
+        if (!$event instanceof Update) {
+            return false;
+        }
+        $data = $event->toArray();
+        $messageChat = Chat::forUser($data['message']['chat']['id'] ?? 0);
+        $messageUser = Chat::forUser($data['message']['from']['id'] ?? 0);
+        return $messageChat->equals($this->chat)
+            && $messageUser->equals($this->user);
     }
 }
